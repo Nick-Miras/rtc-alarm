714ce580b6688a789ecf95dd1f46ad3e5f9add6a
Nick Miras <mirasnickanthony@gmail.com>
2025-01-16 14:53:26 +0800

    added files for activating alarm and buzzer

---
+++ arduino/src/alarm.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Nick Anthony Miras on 1/16/25.
+//
+
+#include "alarm.h"
+
+
+void alarmInit(void) {}
---
+++ arduino/src/alarm.h
@@ -0,0 +1,10 @@
+//
+// Created by Nick Anthony Miras on 1/16/25.
+//
+
+#ifndef ALARM_H
+#define ALARM_H
+
+extern void alarmInit(void);
+
+#endif //ALARM_H
